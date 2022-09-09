48806436ff34984aea1a5faf7bd9045305a22b3e
Kirill <Kirillashmidt@gmail.com>
2022-09-09 16:31:11 +0300

    Revert "Little fix"
    This reverts commit 2981f2064482f0610888c38d03aeaccdaa99b4e4.

--- MovieTelegramBot/DataContainer.cs
+++ MovieTelegramBot/DataContainer.cs
@@ -2,6 +2,6 @@
 {
     internal class DataContainer
     {
-        public static readonly string TOKEN = "";
+        public static readonly string TOKEN = "5684486563:AAEqNh0SusRtaYY7TN2AgqTie-zBByV6GpA";
     }
 }
