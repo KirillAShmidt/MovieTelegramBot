258d280a2167c924711818e312b165c955034501
Kirill <Kirillashmidt@gmail.com>
2022-09-09 16:32:23 +0300

    little fixes

--- MovieTelegramBot/Program.cs
+++ MovieTelegramBot/Program.cs
@@ -6,27 +6,8 @@ namespace MovieTelegramBot
     {
         static void Main(string[] args)
         {
-            /*using(var context = new MovieContext())
-            {
-                var movie = new Movie()
-                {
-                    Title = "Interstellar",
-                    Year = 2014,
-                    Description = "movie about space"
-                };
-
-                context.Movies.Add(movie);
-                context.SaveChanges();
-
-                var movie1 = context.Movies.Find(1);
-
-                //Console.WriteLine($"id: {movie1.Id} | Title: {movie1.Title}");
-                Console.ReadLine();*/
-
             var bot = new BotManager();
-
             bot.InitializeBot();
-
 
             Console.ReadLine();
         }
